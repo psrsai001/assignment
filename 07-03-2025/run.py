9baef9866e5637a9636e2d6add70b56c217f3381
psrsai001 <psrsai001.placements@gmail.com>
2025-03-07 15:53:35 +0530

    added env vars support

--- 07-03-2025/run.py
+++ 07-03-2025/run.py
@@ -1,8 +1,14 @@
+import os
+
 from blueprints.app import create_app, db
+
+ENV = os.getenv("ENV", "development")
+DEBUG = bool(os.getenv("DEBUG", True if ENV == "development" else False))
+PORT = int(os.getenv("PORT", 3000))
 
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", debug=True, port=3000)
+    app.run(host="0.0.0.0", debug=DEBUG, port=PORT)
